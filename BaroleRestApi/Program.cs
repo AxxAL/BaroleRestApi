34f44fcf8639450aba9270f79c1335863b24fb25
Axel <axxal@protonmail.ch>
2022-02-27 23:56:30 +0100

    ConnectionString

--- BaroleRestApi/Program.cs
+++ BaroleRestApi/Program.cs
@@ -1,13 +1,8 @@
-using System;
 using BaroleRestApi.Data;
 using BaroleRestApi.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
